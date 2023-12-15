f5c33101c35d5f31ed401028b274974d78ef8a8b
vaskaraminashvili <vaska.r02@gmail.com>
2023-12-15 18:36:26 +0400

    wip

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Some default layout content shared across all pages</p>
+    <slot />
+  </div>
+</template>
+
+<script>
+import "~/public/assets/css/theme.css"
+export default {
+
+}
+</script>
